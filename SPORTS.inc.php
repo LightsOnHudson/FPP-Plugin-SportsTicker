9703bb19c89d369aed58a7d804e48b94319240cf
patdelaney <pat_delaney@colonydrive.com>
2022-09-19 19:14:46 -0500

    Update year to 2022 for mlb

--- SPORTS.inc.php
+++ SPORTS.inc.php
@@ -1,7 +1,7 @@
 <?php 
 
 $SPORTS_DATA_ARRAY= array(array("NFL","http://sports.espn.go.com/nfl/bottomline/scores"),
-					array("MLB","http://gd2.mlb.com/components/game/mlb/year_2017/month_05/day_05/master_scoreboard.json"),
+					array("MLB","http://gd2.mlb.com/components/game/mlb/year_2022/month_05/day_05/master_scoreboard.json"),
 					array("NCF" ,"http://sports.espn.go.com/ncf/bottomline/scores"));
 
 //MLB: http://gd2.mlb.com/components/game/mlb/year_2017/month_05/day_05/scoreboard.xml
